9694f26be06f51eafcb2b297eddd0cf568e96bae
artiow <namednev.artem@gmail.com>
2020-03-19 10:55:39 +0300

    Candle value type fix

--- client-api/src/main/java/com/artiow/moex/api/model/Candle.java
+++ client-api/src/main/java/com/artiow/moex/api/model/Candle.java
@@ -3,18 +3,19 @@ package com.artiow.moex.api.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class Candle {
 
-    private Double open;
-    private Double close;
-    private Double high;
-    private Double low;
-    private Double value;
-    private Double volume;
+    private BigDecimal open;
+    private BigDecimal close;
+    private BigDecimal high;
+    private BigDecimal low;
+    private BigDecimal value;
+    private BigDecimal volume;
     private LocalDateTime begin;
     private LocalDateTime end;
 }
